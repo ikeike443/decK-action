a7b1e59d9b6fde052bb157f93ab986873c2d5742
Takafumi Ikeda <ikeike443@gmail.com>
2020-03-17 17:45:15 +0900

    trying to pull deck image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.10
+FROM hbagdi/deck
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,5 @@
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
+
+deck ping --kong-addr "http://ikeike443-demo.net:8001" 
