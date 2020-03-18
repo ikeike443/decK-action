22c2ee7d382cc3c51c2b416b085ab3637a161d24
Takafumi Ikeda <ikeike443@gmail.com>
2020-03-18 20:11:18 +0900

    set default value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 # deck validate command
-deck_validate (){
+deck_execute (){
     dir=$1
     ops=$2
     echo $dir
@@ -12,39 +12,12 @@ deck_validate (){
     done
 }
 
-deck_sync (){
-    dir=$1
-    ops=$2
-    echo $dir
-    echo $ops
-    for file in $(ls $dir); do
-        echo $dir/$file
-        deck sync ${ops} -s $dir/$file
-    done
-}
-
-deck_diff (){
-    dir=$1
-    ops=$2
-    echo $dir
-    echo $ops
-    for file in $(ls $dir); do
-        echo $dir/$file
-        deck diff ${ops} -s $dir/$file
-    done
-}
-
-
 case $1 in
-    "validate") deck_validate $3 "$2" ;;
-    "diff") deck_diff $3 "$2" ;;
-    "sync") deck_sync $3 "$2" ;;
-    * ) deck $1 $2 ;;
+    "validate") deck_execute $2 "$3" ;;
+    "diff") deck_execute $2 "$3" ;;
+    "sync") deck_execute $2 "$3" ;;
+    * ) deck $1 $3 ;;
 esac
 
 
-
-# TODO: sync -> マージ時にKongとSync
-# TODO: diff -> 何らかの形でPRへフィードバック
-# TODO: validate -> PRのコードレビュー画面の該当行へフィードバック
 # TODO: 引数がなくても動くように
