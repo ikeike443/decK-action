de3ea6a273dce513d156cd76b00176eb29ae9c8f
Takafumi Ikeda <ikeike443@gmail.com>
2020-03-18 19:12:57 +0900

    implemented vaidate, dif, sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,47 @@
 #!/bin/sh -l
 
-deck ping --kong-addr $1 --headers $2
+# deck validate command
+deck_validate (){
+    dir=$1
+    ops=$2
+    echo $dir
+    echo $ops
+    for file in $(ls $dir); do
+        echo $dir/$file
+        deck validate ${ops} -s $dir/$file
+    done
+}
+
+deck_sync (){
+    dir=$1
+    ops=$2
+    echo $dir
+    echo $ops
+    for file in $(ls $dir); do
+        echo $dir/$file
+        deck sync ${ops} -s $dir/$file
+    done
+}
+
+deck_diff (){
+    dir=$1
+    ops=$2
+    echo $dir
+    echo $ops
+    for file in $(ls $dir); do
+        echo $dir/$file
+        deck diff ${ops} -s $dir/$file
+    done
+}
+
+
+case $1 in
+    "validate") deck_validate $3 "$2" ;;
+    "diff") deck_diff $3 "$2" ;;
+    "sync") deck_sync $3 "$2" ;;
+    * ) deck $1 $2 ;;
+esac
+
 
 
 # TODO: sync -> マージ時にKongとSync
