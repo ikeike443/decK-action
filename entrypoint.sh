e2d8059aaf0c5cbe2867036a42bf868d5b938010
Takafumi Ikeda <ikeike443@gmail.com>
2020-03-18 21:05:07 +0900

    fixed funny bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 set -e -o pipefail
 
 # deck validate command
-deck_execute (){
-    dir=$1
-    ops=$2
+deck_multi_execute (){
+    cmd=$1
+    dir=$2
+    ops=$3
     if [ ! -e ${dir} ]; then
         echo "${dir}: No such file or directoy exists";
         exit 1;
@@ -13,17 +14,19 @@ deck_execute (){
     for file in $(ls $dir); do
         echo $dir/$file
 
-        deck validate ${ops} -s $dir/$file
+        deck $cmd $ops -s $dir/$file
     done
 }
 
 case $1 in
-    "validate") deck_execute $2 "$3" ;;
-    "diff") deck_execute $2 "$3" ;;
-    "sync") deck_execute $2 "$3" ;;
+    "validate") deck_multi_execute $1 $2 "$3" ;;
+    "diff") deck_multi_execute $1 $2 "$3" ;;
+    "sync") deck_multi_execute $1 $2 "$3" ;;
     * ) deck $1 $3 ;;
 esac
 
 
 
 # TODO: 引数がなくても動くように
+# TODO: Sync時にDeploy API叩く
+# TODO: diffでComment入れる
