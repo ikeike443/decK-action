d1e9d5d5754a33c45b140c035fcec7bc9a6433e6
Takafumi Ikeda <ikeike443@gmail.com>
2020-03-18 17:09:24 +0900

    added workflow files to test myself

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 
 deck ping --kong-addr $1 --headers $2
+
+
+# TODO: sync -> マージ時にKongとSync
+# TODO: diff -> 何らかの形でPRへフィードバック
+# TODO: validate -> PRのコードレビュー画面の該当行へフィードバック
+# TODO: 引数がなくても動くように
