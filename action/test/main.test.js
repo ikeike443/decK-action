c52ba9d2ef81f2b70aac500dbc7dccdded54b08a
Takafumi Ikeda <ikeike443@gmail.com>
2020-05-17 21:43:32 +0900

    Deleted kong directory

--- action/test/main.test.js
+++ action/test/main.test.js
@@ -36,34 +36,9 @@
         
     });
 
-    const spyLog = jest.spyOn(console, 'log');
-    spyLog.mockImplementation(x => x);
-    
-    test ("Test deck validate", async () => {
-        const ghcontext = new GHContext(env.GITHUB_TOKEN,"jest","pull_request","./action/test/github/event.json", "ikeike443/decK-action-local", "");
-        
-        const deck = new decK("kong", "", ghcontext)
- 
-        const data = await deck.validate();
-        
-        expect(console.log).toBeCalled();
-        expect(spyLog.mock.calls[14][0]).toEqual('Executing: deck  validate  -s kong/default.yaml');
-        expect(spyLog.mock.calls[15][0]).toEqual('');
-    });
-
-    test ("Test deck diff", async () => {
-        const ghcontext = new GHContext(env.GITHUB_TOKEN,"jest","pull_request","./action/test/github/event.json", "ikeike443/decK-action-local", "");
-        
-        const deck = new decK("kong", "", ghcontext)
-
-        const data = await deck.diff();
-        
-        expect(console.log).toBeCalled();
-        expect(spyLog.mock.calls[30][0]).toEqual('Executing: deck  diff  -s kong/default.yaml');
-    });
 
     test ("Test if it gets commit files", async () => {
-        const ghcontext = new GHContext(env.GITHUB_TOKEN,"jest","push","./action/test/github/event.json", "ikeike443/decK-action-local", "fe36b70c67bc2c86cb1220b8a203844997d4c338");
+        const ghcontext = new GHContext(env.GITHUB_TOKEN,"jest","push","./action/test/github/event.json", "ikeike443/decK-action", "fe36b70c67bc2c86cb1220b8a203844997d4c338");
             
         const data = await ghcontext.get_files();
         
@@ -103,23 +78,6 @@
 
         expect(result).toStrictEqual(
             [
-                ".github/workflows/PR.yml",
-                ".github/workflows/PUSH.yml",
-                ".github/workflows/main.yaml",
-                "action/src/main.js",
-                "action/src/sum.js",
-                "action/test/github/event.json",
-                "action/test/kong/Audi.yaml",
-                "action/test/kong/BMW.yaml",
-                "action/test/kong/Ford.yaml",
-                "action/test/kong/Mercedes.yaml",
-                "action/test/kong/Opel.yaml",
-                "action/test/kong/Porsche.yaml",
-                "action/test/kong/Volkswagen.yaml",
-                "action/test/kong/default.yaml",
-                "action/test/main.test.js",
-                "action/test/sum.test.js",
-                "package.json",
             ]
         );
         
